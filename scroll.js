9406b71febff6f0bdacb2e5b2d08dc22325205e6
Eugene Shulgin <eugene.shulgin@mail.ru>
2022-12-18 11:46:29 +0300

    add form + contacts

--- scroll.js
+++ scroll.js
@@ -1,8 +1,18 @@
-const scrollBtn = document.querySelector('.isShowBtn')
-window.onscroll = () => {
-	if(window.scrollY > 500){
-		scrollBtn.classList.remove('.isShowBtn__hide');
-	} else if (window.scrollY < 700){
-		scrollBtn.classList.add('.isShowBtn');
+const goTopBtn = document.querySelector('.goTop');
+
+goTopBtn.addEventListener('click', goTop);
+window.addEventListener('scroll', trackScroll);
+
+function trackScroll(){
+	if(window.scrollY > 300){
+		goTopBtn.classList.add('goTop__show');
+	} else if (window.scrollY < 300){
+		goTopBtn.classList.remove('goTop__show');
+	}
+}
+function goTop (){
+	if(window.pageYOffset > 0){
+		window.scrollBy(0, -75);
+		setTimeout(goTop, 0)
 	}
 }
