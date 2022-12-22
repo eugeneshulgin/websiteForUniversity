64c319ecc905f334bee128cf19649f8d41393ffd
Eugene Shulgin <eugene.shulgin@mail.ru>
2022-12-22 23:39:04 +0300

    add fixed bags

---
+++ js/shar.js
@@ -0,0 +1,66 @@
+let sharBah = document.querySelector('.sharBah');
+
+sharBah.addEventListener('click', () => {
+	sharBah.classList = 'shar bah';
+	setTimeout(function(){
+		sharBah.classList = 'deleteShar'
+	}, 700)
+ 
+ })
+
+
+///////////////////////////////////////////////////////
+ let sharBah2 = document.querySelector('.sharBah2');
+
+ sharBah2.addEventListener('click', () => {
+	sharBah2.classList = 'shar2 bah'
+ 
+	setTimeout(function(){
+		sharBah2.classList = 'deleteShar'
+	}, 700)
+ 
+ })
+ ///////////////////////////////////////////////////////
+ let sharBah3 = document.querySelector('.sharBah3');
+
+ sharBah3.addEventListener('click', () => {
+	sharBah3.classList = 'shar3 bah'
+ 
+	setTimeout(function(){
+		sharBah3.classList = 'deleteShar'
+	}, 700)
+ 
+ })
+ ///////////////////////////////////////////////////////
+ let sharBah4 = document.querySelector('.sharBah4');
+
+ sharBah4.addEventListener('click', () => {
+	sharBah4.classList = 'shar4 bah'
+ 
+	setTimeout(function(){
+		sharBah4.classList = 'deleteShar'
+	}, 700)
+ 
+ })
+ ///////////////////////////////////////////////////////
+ let sharBah5 = document.querySelector('.sharBah5');
+
+ sharBah5.addEventListener('click', () => {
+	sharBah5.classList = 'shar5 bah'
+ 
+	setTimeout(function(){
+		sharBah5.classList = 'deleteShar'
+	}, 700)
+ 
+ })
+ ///////////////////////////////////////////////////////
+ let sharBah6 = document.querySelector('.sharBah6');
+
+ sharBah6.addEventListener('click', () => {
+	sharBah6.classList = 'shar6 bah'
+ 
+	setTimeout(function(){
+		sharBah6.classList = 'deleteShar'
+	}, 700)
+ 
+ })
